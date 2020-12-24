f00c8df07771d7ff80f67344f6a4897b4bd0f9d0
frovai <34864119+frovai@users.noreply.github.com>
2020-12-24 14:22:44 -0300

    Update nginx-restart-when-down.sh

--- nginx-restart-when-down.sh
+++ nginx-restart-when-down.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 date=$(date)
 serv=nginx
-sstat=Stopped
-service $serv status | grep -i 'running\|stopped' | awk '{print $6}' | while read output;
+sstat='(dead)'
+service $serv status | grep -i 'running\|dead' | awk '{print $3}' | while read output;
 do
 echo $output
 if [ "$output" == "$sstat" ]; then
     service $serv start
     echo "$serv service is UP now.! at $date" >> /home/ubuntu/restart-nginx.log
     else
-    echo "$serv service is running"
+    echo "$serv service nginx is running" >> /home/ubuntu/restart-nginx.log
     fi
 done
